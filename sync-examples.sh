95831707fa8429e31ae455664cab743d9de50e27
flavioespinoza <flavio.espinoza@gmail.com>
2020-05-04 17:49:36 -0600

    sync-examples.sh script

---
+++ sync-examples.sh
@@ -0,0 +1,52 @@
+green='\033[32m'
+end_green='\033[0m'
+            
+red='\033[31m'
+end_red='\033[0m'
+            
+yellow='\033[33m'
+end_yellow='\033[0m'
+      
+blue='\033[34m'  
+end_blue='\033[0m'
+
+cyan='\033[36m'
+end_cyan='\033[0m'
+
+# vars -------
+timestamp=$(date +%s)
+
+
+echo -e "${green}starting sync${end_green}"
+
+cd ..
+
+rm -rf tempest--*
+
+git clone https://github.com/flavioespinoza/docker-react-node.git tempest--${timestamp}
+
+cp sync-docs/examples tempest--${timestamp}
+
+cd tempest--${timestamp}
+
+git checkout -b tempest--${timestamp}
+
+git add .
+
+git commit 'Sync-Docs - Copy Examples'
+
+git push -u origin tempest--${timestamp}
+
+hub pull-request -m "Sync-Docs: tempest--${timestamp}"
+
+cd ..
+
+rm -rf tempest--${timestamp}
+
+echo -e "${green}sync complete${end_green}"
+
+# if [ -z "$(git status --porcelain)" ]; then 
+#   # Working directory clean
+# else 
+#   # Uncommitted changes
+# fi
